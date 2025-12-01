f2cab7fdb154885df6a060ae5b2b7dffaa9395c9
Bryan Packer <bryanpacker74@gmail.com>
2025-11-30 22:20:33 -0300

    fix: changing critical bug of remote backend

--- infra-emergency/main.tf
+++ infra-emergency/main.tf
@@ -5,11 +5,11 @@ terraform {
       version = "5.46"
     }
   }
-  required_version = "1.13.1"
+  required_version = "1.13.5"
 
   backend "s3" {
     bucket         = "hackathon-devs2blu-terraform-state-1533"
-    key            = "hackathon/terraform.tfstate"
+    key            = "emergency/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "hackathon-terraform-locks"
     encrypt        = true
