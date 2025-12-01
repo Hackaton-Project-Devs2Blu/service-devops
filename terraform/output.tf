54bb8da323a5dfeeb6b1bf67728887c10c9291e2
Bryan Packer <bryanpacker74@gmail.com>
2025-12-01 00:22:30 -0300

    fix: outputs terraform

--- terraform/output.tf
+++ terraform/output.tf
@@ -18,3 +18,8 @@ output "private_subnets" {
   description = "Private Subnets IDs (For RDS)"
   value       = module.vpc.private_subnets
 }
+
+output "url_aplicacao" {
+  description = "Acesse a aplicação aqui (Load Balancer)"
+  value       = "http://${module.alb.dns_name}"
+}
